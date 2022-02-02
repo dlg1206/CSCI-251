1d27dd35b2e93ae699171c6fa9090bd05f4c9e36
Derek Garcia <dlg1206@g.rit.edu>
2022-02-02 13:11:24 -0500

    Seconds output correctly; input args fixed; fixed negative byte display

--- Project1/du/Program.cs
+++ Project1/du/Program.cs
@@ -10,8 +10,8 @@ namespace du
         public static void Main(string[] args)
         {
             // todo account for spaces? ie "-b Program Files" -> [-b, Program, Files-
-            var input = args[0].Split(" ");
-            if (args.Length == 0 || !ValArgs(input))
+            
+            if (!ValArgs(args))
             {
                 Console.WriteLine("Usage: du [-s] [-p] [-b] <path>");
                 Console.WriteLine("Summarize disk usage of the set of FILEs, recursively for directories.\n");
@@ -24,21 +24,21 @@ namespace du
             }
             var sw = new Stopwatch();
             // Deter commands
-            switch (input[0])
+            switch (args[0])
             {
                 case "-s":
                     sw.Start();
-                    int[] info = ParseSeq(input[1], new int[3]);
+                    uint[] info = ParseSeq(args[1], new uint[3]);
                     sw.Stop();
                     PrintResults(sw, "Sequential", info);
                     break;
                 case "-p":
-                    ParsePar(input[1]);
+                    ParsePar(args[1]);
                     break;
                 case "-b":
                     // todo order? err says seq then par, but demo shows par then seq : PAR -> SEQ
-                    ParseSeq(input[1], new int[3]);
-                    ParsePar(input[1]);
+                    ParseSeq(args[1], new uint[3]);
+                    ParsePar(args[1]);
                     break;
                 default:
                     return;
@@ -70,11 +70,11 @@ namespace du
           
         }
 
-        private static void PrintResults(Stopwatch sw, string mode, int[] info)
+        private static void PrintResults(Stopwatch sw, string mode, uint[] info)
         {
             // .elapsed
-            var elapsed = String.Format("{0:0}.{1:0}", sw.Elapsed.Seconds, sw.Elapsed.Milliseconds / 10);
-            Console.WriteLine("\n" + mode + " Calculated in: " + elapsed + "s" );
+
+            Console.WriteLine("\n" + mode + " Calculated in: {0}s, ", sw.Elapsed );
             Console.WriteLine("{0:n0} folders, {1:n0} files, {2:n0} bytes\n", info[0], info[1], info[2]);
         }
 
@@ -88,7 +88,7 @@ namespace du
         /// <param name="src"></param>
         /// <param name="info"></param>
         /// <returns></returns>
-        private static int[] ParseSeq(string src, int[] info)
+        private static uint[] ParseSeq(string src, uint[] info)
         {
             try
             {
@@ -115,7 +115,7 @@ namespace du
                 try
                 {
                     var file = File.Open(fileName, FileMode.Open);
-                    info[2] += (int) file.Length;
+                    info[2] += (uint) file.Length;
                     file.Close();
                 }
                 catch(Exception)
