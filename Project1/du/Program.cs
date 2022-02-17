72c3e2d2bac1b2e225b171b5a36a3da3d4068eb3
Derek Garcia <dlg1206@g.rit.edu>
2022-02-17 15:02:26 -0500

    Initial fix of project1

--- Project1/du/Program.cs
+++ Project1/du/Program.cs
@@ -25,7 +25,7 @@ namespace du
                 Console.WriteLine("         Runs sequential followed by parallel mode");
                 return;
             }
-            
+
             // Switches based on given summary method
             switch (args[0])
             {
@@ -35,19 +35,21 @@ namespace du
                     break;
                 // Parallel
                 case "-p":
-                    DoPar(args[1]);
+                    var p = new ParallelSearch();
+                    p.DoPar(args[1]);
                     break;
                 // Both
                 case "-b":
+                    var pb = new ParallelSearch();
+                    pb.DoPar(args[1]);
                     DoSeq(args[1]);
-                    DoPar(args[1]);
                     break;
                 default:
                     return;
             }
         }
-        
-        
+
+
         /// <summary>
         /// Validate input arguments
         /// </summary>
@@ -56,11 +58,17 @@ namespace du
         private static bool ValArgs(string[] args)
         {
             // Only expecting 2 args
-            if (args.Length != 2) { return false; }
-   
+            if (args.Length != 2)
+            {
+                return false;
+            }
+
             // Check if directory exists
-            if (!Directory.Exists(args[1])) { return false; }
-        
+            if (!Directory.Exists(args[1]))
+            {
+                return false;
+            }
+
             // Check if valid parse method
             switch (args[0])
             {
@@ -77,10 +85,10 @@ namespace du
                 default:
                     return false;
             }
-          
+
         }
-        
-        
+
+
         /// <summary>
         /// Encompasses stopwatch and output for Sequential parsing
         /// </summary>
@@ -89,25 +97,14 @@ namespace du
         {
             var sw = new Stopwatch();
             sw.Start();
-            var info = ParseSeq(src, new long[3]);  // Sequential Parsing
+            var info = ParseSeq(src, new long[3]); // Sequential Parsing
             sw.Stop();
             PrintResults(sw, "Sequential", info);
         }
+
+
         
-        
-        /// <summary>
-        /// Encompasses stopwatch and output for Parallel parsing
-        /// </summary>
-        /// <param name="src"></param>
-        private static void DoPar(string src)
-        {
-            var sw = new Stopwatch();
-            sw.Start();
-            var info = ParsePar(src, new long[3]);  // Parallel Parsing
-            sw.Stop();
-            PrintResults(sw, "Parallel", info);
-        }
-        
+
         /// <summary>
         /// Print the results of a parse search
         /// </summary>
@@ -116,11 +113,11 @@ namespace du
         /// <param name="info">data with directory, file, and byte information</param>
         private static void PrintResults(Stopwatch sw, string mode, long[] info)
         {
-            Console.WriteLine("\n" + mode + " Calculated in: {0}s, ", sw.Elapsed );
+            Console.WriteLine("\n" + mode + " Calculated in: {0}s, ", sw.Elapsed);
             Console.WriteLine("{0:n0} folders, {1:n0} files, {2:n0} bytes\n", info[0], info[1], info[2]);
         }
 
-        
+
         /// <summary>
         /// Sequentially parse with recursion
         /// </summary>
@@ -136,104 +133,115 @@ namespace du
                 // Recursively go through directories
                 foreach (var dir in Directory.GetDirectories(src))
                 {
-                    info[0]++;      // update directory count
+                    info[0]++; // update directory count
                     ParseSeq(dir, info);
                 }
             }
             // Catch if unable to open directory
             catch (Exception)
             {
-               
+
             }
-            
+
             // Count each file once no more directories to go into
             foreach (var fileName in Directory.GetFiles(src))
             {
-                info[1]++;  // update file count
-                
+                info[1]++; // update file count
+
                 // Attempt to open file
                 try
                 {
                     var file = File.Open(fileName, FileMode.Open);
-                    info[2] += file.Length;     // update byte count
+                    info[2] += file.Length; // update byte count
                     file.Close();
-                }
-                // Catch if unable to open file
-                catch(Exception)
-                {
-                    
-                }
-            }
-            
-            // Return the updated information
-            return info;
-        }
-        
-        
-        private static Object _parLock = new Object();          // Lock for threads
-        /// <summary>
-        /// Parallel parse with recursion
-        /// </summary>
-        /// <param name="src">root directory</param>
-        /// <param name="info">data with directory, file, and byte information</param>
-        /// <returns>data with directory, file, and byte information</returns>
-        private static long[] ParsePar(string src, long[] info)
-        {
-            // Attempt to open directory
-            try
-            {
-
-                Directory.SetCurrentDirectory(src);
-                
-                Parallel.ForEach(Directory.GetDirectories(src), dir =>
-                {
-                    // Update directory count
-                    lock (_parLock)
-                    {
-                        info[0]++;
-                    }
-                    
-                    // Parse next directory
-                    ParseSeq(dir, info);
-                });
-            }
-            // Catch if unable to open directory
-            catch (Exception)
-            {
-               
-            }
-            
-            // Count each file once no more directories to go into
-            Parallel.ForEach(Directory.GetFiles(src), fileName =>
-            {
-                // Update file count
-                lock (_parLock)
-                {
-                    info[1]++;
-                }
-                
-                // Attempt to open file
-                try
-                {
-                    // Open and read file
-                    
-                    lock (_parLock)
-                    {
-                        var file = File.Open(fileName, FileMode.Open);
-                        info[2] += file.Length;     // update byte count
-                        file.Close();
-                    }
-                    
                 }
                 // Catch if unable to open file
                 catch (Exception)
                 {
 
                 }
-            });
-            
+            }
+
             // Return the updated information
             return info;
         }
+        
+        private class ParallelSearch
+        {
+            private int _fileCount ;
+            private int _folderCount;
+            private long _byteCount;
+            
+            /// <summary>
+            /// Encompasses stopwatch and output for Parallel parsing
+            /// </summary>
+            /// <param name="src"></param>
+            public void DoPar(string src)
+            {
+                var sw = new Stopwatch();
+                sw.Start();
+                ParsePar(src); // Parallel Parsing
+                sw.Stop();
+
+                // Print Results
+                Console.WriteLine("\n Parallel Calculated in: {0}s, ", sw.Elapsed);
+                Console.WriteLine("{0:n0} folders, {1:n0} files, {2:n0} bytes\n", _fileCount, _folderCount, _byteCount);
+            }
+
+            
+            /// <summary>
+            /// Parallel parse with recursion
+            /// </summary>
+            /// <param name="src">root directory</param>
+            /// <returns>data with directory, file, and byte information</returns>
+            private void ParsePar(string src)
+            {
+                // Directory parsing
+                try
+                {
+                    // Attempt to open directory
+                    var di = new DirectoryInfo(src);
+                    Parallel.ForEach(di.GetDirectories(), dir =>
+                    {
+                        // Update directory count
+                        Interlocked.Add(ref _folderCount, 1);
+
+                        // Parse next directory
+                        ParsePar(di.Name);
+                    });
+                }
+                // Catch if unable to open directory
+                catch (Exception)
+                {
+
+                }
+                
+                // Files and File sizes
+                try
+                {
+                    // Attempt to open directory
+                    var di = new DirectoryInfo(src);
+
+                    // Count each file once no more directories to go into
+                    Parallel.ForEach(di.GetFiles(), file =>
+                    {
+                        // TODO what does ref mean?
+
+                        Interlocked.Add(ref _fileCount, 1); // Update file count
+
+                        Interlocked.Add(ref _byteCount, file.Length); // update byte count
+
+                    });
+
+                }
+                catch(Exception)
+                {
+                    
+                }
+            }
+        }
+
+
+        
     }
 }
