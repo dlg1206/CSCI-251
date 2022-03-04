b25303bd983f690b78560d0e9aefb10b60caa3ff
Derek Garcia <dlg1206@g.rit.edu>
2022-03-04 01:52:34 -0500

    more docstrings

--- Project 2/PrimeGen/Program.cs
+++ Project 2/PrimeGen/Program.cs
@@ -1,5 +1,9 @@
-﻿/// @author Derek Garcia
-
+﻿/*
+ * Project 2
+ * Finds Primes of a given bit size
+ * 
+ * @author Derek Garcia
+ */
 
 using System.Diagnostics;
 using System.Numerics;
@@ -8,15 +12,18 @@ using System.Security.Cryptography;
 
 namespace PrimeGen
 {
-    
-
-    
-    /// <summary>
-    /// Main Function that takes in args
-    /// </summary>
     public static class Program
     {
+        // For error checking
         private const int MaxArgs = 2;
+        
+        // For FindPrime
+        private const int BitsPerByte = 8;
+        
+        // For Miller-Rabin Algorithm
+        private const bool Composite = false;
+        private const bool ProbablyPrime = true;
+
         /// <summary>
         /// Main that takes in args
         /// </summary>
@@ -29,37 +36,46 @@ namespace PrimeGen
                 PrintUsage();
                 return;
             }
-
+            // todo check if numBits mult of 8, min 32
+            // todo dotnet run 32 ok? count default to 1
             // Check both inputs are valid ints
-            int numBits, count;
-            if (!int.TryParse(args[0], out numBits) || !int.TryParse(args[1], out count))
+            if (!int.TryParse(args[0], out var numBits) || !int.TryParse(args[1], out var count))
             {
                 PrintUsage();
                 return;
             }
-
+            
+            // Write bit information
             Console.WriteLine("BitLength: {0} bits", numBits);
             
+            // start timer
             var sw = new Stopwatch();
-
             sw.Start();
-            for (int i = 0; i < count; i++)
+            
+            // Get desired number of primes
+            for (int i = 1; i <= count; i++)
             {
                 var prime = FindPrime(numBits);
-                Console.WriteLine("{0}: {1}", i+1, prime);    // print prime
-
+                Console.WriteLine("{0}: {1}", i, prime);    // print prime
+                
+                // print newline if not final prime to print
+                if(i < count)
+                    Console.WriteLine();
+                
+                // todo REMOVE THIS WHEN SUBMITTING
                 if (prime % new BigInteger(2) == 0)
                 {
                     Console.WriteLine("\tEVEN");
                 }
             }
-            // TODO doesn't terminate and reach here
+            // TODO Performance issues; 1 8192 ran several minutes and nothing
+            // Stop timer and report elapsed time
             sw.Stop();
             Console.WriteLine("Time to Generate: {0}", sw.Elapsed);
         }
 
         /// <summary>
-        /// Prints usage of the program
+        /// Prints the correct usage of the program
         /// </summary>
         private static void PrintUsage()
         {
@@ -69,110 +85,59 @@ namespace PrimeGen
             Console.WriteLine("\t- count - the number of prime numbers to generate, defaults to 1");
         }
 
-        private static object _lock = new object();
+        
+        private static readonly  object Lock = new object();    // for use in assigning primes
         /// <summary>
-        /// Finds numerous primes
+        /// Finds a single probably prime number
         /// </summary>
         /// <param name="numBits">size of the prime</param>
-        /// <returns>Prime number of give bits</returns>
+        /// <returns>A probably prime number</returns>
         private static BigInteger? FindPrime(int numBits)
         {
-            
             // init vars
             BigInteger? prime = null;
-            var bytes = numBits / 8;
+            var bytes = numBits / BitsPerByte;  // convert bits to bytes
             var rng = RandomNumberGenerator.Create();
 
-            // While number of primes not found, keep checking
+            // Loop until fine a probably prime number
             Parallel.For(0, Int32.MaxValue, (i, state) =>
             {
-                // If unlocked bi is changed while checking
-         
-                var numBytes = new byte[bytes];   // convert bits to bytes
-                // Make a randing big int
-                //Console.WriteLine("making new bi");
+                
+                // Make random BigInteger
+                var numBytes = new byte[bytes];
                 rng.GetBytes(numBytes);
                 BigInteger bi = new BigInteger(numBytes);
-                bi = BigInteger.Abs(bi);
-               // Console.WriteLine("made new bi");
+                bi = BigInteger.Abs(bi);    // abs so only positive values
+
+                // If rand BigInt fails quick check or Miller-Rabin test, don't continue
+                if (!bi.InitialPrimeCheck() || !bi.IsProbablyPrime()) return;
                 
+                // If reach here then bi is valid prime
+                lock (Lock)
+                    prime ??= bi;   // Assign value of bi to prime if its null
                 
-                if (bi.InitialPrimeCheck() && bi.IsProbablyPrime())
-                {
-                    // Just for printing
-                    // lock (_lock)
-                    // {
-                    //     Console.WriteLine("{0} has found prime {1}", Task.CurrentId, bi);
-                    //     Console.WriteLine("\t{0} init check result for {1}: {2}", Task.CurrentId, bi ,bi.InitialPrimeCheck());
-                    //     Console.WriteLine("\t{0} Is prob check result for {1}: {2}", Task.CurrentId, bi, bi.IsProbablyPrime());
-                    // }
-                    
-
-                    lock (_lock)
-                    {
-                        if (prime == null)
-                        {
-                            
-                            //Console.WriteLine("\t\tbi of t{0} is {1}", Task.CurrentId ,bi);
-                            // todo bi changes from above to below
-                            prime = bi;
-                           // Console.WriteLine("\t\t{0} has assigned prime to {1}", Task.CurrentId, prime);
-                            
-                            
-                        }
-
-                    }
-                    
-                    state.Stop();
-                    
-                }
-                else
-                {
-                   // Console.WriteLine("\tfailed");
-                }
-             
-
+                // prime has been assigned, can stop threads
+                state.Stop();
             });
-            //Console.WriteLine("\t\tReturning prime {0}", prime);
+            
+            // return prime that was found
             return prime;
         }
 
 
+        /// <summary>
+        /// Extension method for a BigInteger that does an initial prime test
+        /// by dividing the value by the first 15 primes
+        /// </summary>
+        /// <param name="value">value to test if prime</param>
+        /// <returns>true if passes, false otherwise</returns>
         private static bool InitialPrimeCheck(this BigInteger value)
         {
-            
             // 2 is the only even prime number
             if (value == 2)
-            {
                 return true;
-            }
             
-            // // check if can divide by other primes
-            // if (value % new BigInteger(2) == 0)
-            // {
-            //     return false;
-            // }
-            //
-            // if (value % new BigInteger(3) == 0)
-            // {
-            //     return false;
-            // }
-            //
-            // if (value % new BigInteger(5) == 0)
-            // {
-            //     return false;
-            // }
-            //
-            // if (value % new BigInteger(7) == 0)
-            // {
-            //     return false;
-            // }
-            //
-            // if (value % new BigInteger(11) == 0)
-            // {
-            //     return false;
-            // }
-
+            // Check if value can be divided by the first 15 primes
             return (value % new BigInteger(2) != 0 &&
                     value % new BigInteger(3) != 0 &&
                     value % new BigInteger(5) != 0 &&
@@ -182,36 +147,44 @@ namespace PrimeGen
                     value % new BigInteger(17) != 0 &&
                     value % new BigInteger(19) != 0 &&
                     value % new BigInteger(23) != 0 &&
-                    value % new BigInteger(29) != 0);
+                    value % new BigInteger(29) != 0 &&
+                    value % new BigInteger(31) != 0 &&
+                    value % new BigInteger(37) != 0 &&
+                    value % new BigInteger(41) != 0 &&
+                    value % new BigInteger(43) != 0 &&
+                    value % new BigInteger(47) != 0);
             
         }
         
+        
         /// <summary>
-        /// Miller–Rabin algorithm to test if the given number is probably
-        /// prime
+        /// Miller–Rabin algorithm to test if the given 
+        /// number is probably prime
         /// </summary>
         /// <param name="n">number to test</param>
         /// <param name="k">number of witnesses</param>
-        /// <returns></returns>
+        /// <returns>true if passes, false otherwise</returns>
         private static bool IsProbablyPrime(this BigInteger n, int k = 10)
         {
+            // Double checks that n is valid for Miller-Rabin
+            
             // 2 is the smallest prime
             if (n < new BigInteger(2))
-                return false;
+                return Composite;
 
             // 2 is only prime that is even
             if (n == 2)
-                return true;
+                return ProbablyPrime;
             
             // != 2 and is even
             if (n != 2 && n % 2 == 0)
-                return false;
+                return Composite;
             
-            // Init values
+            // Begin Miller-Rabin; Init values
             var r = 0;
             var d = n - 1;
 
-            // find largest factor n that is a power of 2
+            // Find an odd value d such that n == 2^r * d + 1
             while (d % 2 == 0)
             {
                 r++;
@@ -233,7 +206,7 @@ namespace PrimeGen
                 // get x value
                 var x = BigInteger.ModPow(a, d, n);
                 
-                // if x == 1 or x == n - 1, continue witnessloop
+                // if x == 1 or x == n - 1, continue Witness Loop
                 if (x == 1 || x == n - 1)
                     continue;
 
@@ -241,12 +214,10 @@ namespace PrimeGen
                 for(var m = 1; m < r; m++)
                 {
                     x = BigInteger.ModPow(x, 2, n);
-
+                   
+                    // if true, return composite
                     if (x == 1)
-                    {
-                        return false;
-                    }
-                        
+                        return Composite;
                     
                     // if true, continue Witness Loop
                     if (x == n - 1)
@@ -254,17 +225,10 @@ namespace PrimeGen
                 }
                 // if true, return composite
                 if (x != n - 1)
-                {
-                    return false;
-                }
-                  
+                    return Composite;
             }
             // Pass all tests, probably prime
-            return true;
+            return ProbablyPrime;
         }
-
-
-        
     }
-    
 }
