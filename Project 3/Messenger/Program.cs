5b9ca730941ea8b8ad388001de7d67fca6c3e2f5
Derek Garcia <dlg1206@g.rit.edu>
2022-03-30 11:43:44 -0400

    main prog that can connect to server

--- Project 3/Messenger/Program.cs
+++ Project 3/Messenger/Program.cs
@@ -6,19 +6,32 @@
  */
 
 
+using System.Net;
+
 namespace Messenger
 {
     public static class Program
     {
         // HttpClient is intended to be instantiated once per application, rather than per-use. See Remarks.
-        static readonly HttpClient client = new HttpClient();
-
-        static async Task Main()
+        public static async Task Main(string[] args)
         {
-            // Call asynchronous network methods in a try/catch block to handle exceptions.
+            var ws = new WebClient();
+
+            await ws.Connect("http://kayrun.cs.rit.edu:5000/Key/jsb@cs.rit.edu");
+        }
+        
+    }
+    
+    public class WebClient
+    {
+        readonly HttpClient Client = new HttpClient();
+
+
+        public async Task Connect(string url)
+        {
             try	
             {
-                HttpResponseMessage response = await client.GetAsync("http://kayrun.cs.rit.edu:5000/Key/jsb@cs.rit.edu");
+                HttpResponseMessage response = await Client.GetAsync(url);
                 response.EnsureSuccessStatusCode();
                 string responseBody = await response.Content.ReadAsStringAsync();
                 // Above three lines can be replaced with new helper method below
@@ -32,7 +45,7 @@ namespace Messenger
                 Console.WriteLine("Message :{0} ",e.Message);
             }
         }
-        
+            
     }
     
 }
