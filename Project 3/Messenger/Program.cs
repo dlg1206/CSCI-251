48989bb03db9ffa8030d1c81c51678c79b10c05a
Derek Garcia <dlg1206@g.rit.edu>
2022-03-30 11:34:24 -0400

    get key from website

--- Project 3/Messenger/Program.cs
+++ Project 3/Messenger/Program.cs
@@ -10,9 +10,29 @@ namespace Messenger
 {
     public static class Program
     {
-        public static void Main(string[] args)
+        // HttpClient is intended to be instantiated once per application, rather than per-use. See Remarks.
+        static readonly HttpClient client = new HttpClient();
+
+        static async Task Main()
         {
-            Console.WriteLine("hello world");
+            // Call asynchronous network methods in a try/catch block to handle exceptions.
+            try	
+            {
+                HttpResponseMessage response = await client.GetAsync("http://kayrun.cs.rit.edu:5000/Key/jsb@cs.rit.edu");
+                response.EnsureSuccessStatusCode();
+                string responseBody = await response.Content.ReadAsStringAsync();
+                // Above three lines can be replaced with new helper method below
+                // string responseBody = await client.GetStringAsync(uri);
+
+                Console.WriteLine(responseBody);
+            }
+            catch(HttpRequestException e)
+            {
+                Console.WriteLine("\nException Caught!");	
+                Console.WriteLine("Message :{0} ",e.Message);
+            }
         }
+        
     }
+    
 }
